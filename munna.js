39ac50030a9348d9f7dc608f7a8931cd43825587
Ashikur Rahman <80146103+Ashikur901@users.noreply.github.com>
2021-03-24 23:19:33 -0700

    Add files via upload

---
+++ munna.js
@@ -0,0 +1,10 @@
+    
+    
+    function  clkbtn () {
+    let input = document.getElementById('stdinput').Value;
+    input = parseInt(input);
+    switch (input) {
+        case  1:
+            document.write('<img src="img/two.jpg">')
+    }
+    }
